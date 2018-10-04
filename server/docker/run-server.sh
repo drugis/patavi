f39d6cd1b8d8609f69e7eb98e9677c0490bb19f8
Daan Reid <osmosisch@gmail.com>
2018-10-04 16:13:23 +0200

    add example run server scritp

---
+++ server/docker/run-server.sh
@@ -0,0 +1,12 @@
+docker run -d --name patavi-server \
+ --link my-rabbit:rabbit \
+ --link postgres:postgres \
+ -e PATAVI_BROKER_HOST=rabbit  \
+ -p 3000:3000 \
+ -e PATAVI_SELF=//localdocker.com:3000 \
+ -e PATAVI_PORT=3000 \
+ -e PATAVI_DB_HOST=postgres \
+ -e PATAVI_DB_NAME=patavitask \
+ -e PATAVI_DB_USER=patavitask \
+ -e PATAVI_DB_PASSWORD=develop \
+ patavi-server
