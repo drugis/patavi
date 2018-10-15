35eb160e0b975013c599089c4a0125daf64f31d3
Joris de Keijser <chinook25@gmail.com>
2018-10-15 14:19:18 +0200

    improve run scripts

--- server/run-server.sh
+++ server/run-server.sh
@@ -1,6 +1,6 @@
 docker run -d --name patavi-server \
   --link my-rabbit:rabbit \
-  --link postgres:postgres \
+  --link postgres \
   -e PATAVI_BROKER_HOST=rabbit \
   -p 3000:3000 \
   -e PATAVI_SELF=//localhost:3000 \
@@ -8,4 +8,5 @@ docker run -d --name patavi-server \
   -e PATAVI_DB_HOST=postgres \
   -e PATAVI_DB_NAME=patavi \
   -e PATAVI_DB_USER=patavi \
-  -e PATAVI_DB_PASSWORD=develop addis/patavi-server
+  -e PATAVI_DB_PASSWORD=develop \
+  addis/patavi-server
