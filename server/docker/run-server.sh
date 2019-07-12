c5093a813b4f5c98bf912e9b58917443a0ad9c3c
karlis <karlis.zalite@gmail.com>
2019-07-12 10:38:59 +0200

    certificates now added during docker run

--- server/docker/run-server.sh
+++ server/docker/run-server.sh
@@ -1,3 +1,15 @@
+cp -r ssl ssl_copy
+sudo chown -R :9999 ssl_copy
+sudo chmod -R 775 ssl_copy
+
+docker volume create patavi_server_certs
+docker run -v patavi_server_certs:/patavi --name helper busybox true
+docker cp ./ssl_copy/server-crt.pem helper:/patavi/
+docker cp ./ssl_copy/server-key.pem helper:/patavi/
+docker rm helper
+
+sudo rm -r ssl_copy
+
 docker run -d --name patavi-server \
   --link my-rabbit:rabbit \
   --link postgres \
@@ -9,4 +21,7 @@ docker run -d --name patavi-server \
   -e PATAVI_DB_NAME=patavi \
   -e PATAVI_DB_USER=patavi \
   -e PATAVI_DB_PASSWORD=develop \
+  --mount source=patavi_server_certs,target=/var/lib/patavi/mount \
   addis/patavi-server
+
+docker exec -ti patavi-server sh -c "cp /var/lib/patavi/mount/* /var/lib/patavi/ssl/"
