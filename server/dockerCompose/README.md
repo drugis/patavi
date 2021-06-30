1863b0f510a9536057dac0c2c24dd3b3b3d74f56
Joris de Keijser <chinook25@gmail.com>
2021-06-30 11:17:31 +0200

    Feature/docker compose (#6)
    Add docker compose setup

---
+++ server/dockerCompose/README.md
@@ -0,0 +1,22 @@
+# Docker compose setup for patavi
+
+This docker compose setup is meant to provide an easier way to get patavi up and running on a personal computer or laptop. It is meant to be used in conjunction with the docker compose setup of our other modules (ADDIS, GeMTC, MCDA).
+
+Prerequisites:
+
+- Have docker installed (https://docs.docker.com/get-docker/).
+- Have docker-compose installed (https://docs.docker.com/compose/install/).
+- Have the files in this folder on your system.
+
+The following components will be ran inside of this compose:
+
+- A Patavi server.
+- A RabbitMQ.
+- A Postgres database.
+- A docker network for the other modules to connect to.
+
+How to run:
+
+In a terminal/command promt type `docker-compose up`. The first time this command is run, all necessary docker images will be downloaded. After this, the setup can be ran without an internet connection by using the same command. If, after the first time, you want to make sure you are using the latest version, use `docker-compose pull` before running `docker-compose up`.
+
+NB: **all services will be run with simple development/testing passwords. This is intended as a local, offline environment. For deployment in secure/online environments, apply different methodology and better security, or contact the developers for advice.**
---
+++ server/dockerCompose/initPataviDB.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+  CREATE USER patavi WITH PASSWORD 'develop';
+  CREATE DATABASE patavi ENCODING 'utf-8' OWNER patavi;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "patavi" --dbname "patavi" <<-EOSQL
+CREATE TYPE patavi_task_status AS ENUM ( 'unknown', 'accepted', 'failed', 'done');
+CREATE TABLE patavi_task (
+  id BIGINT PRIMARY KEY, -- flake ID
+  creator_name VARCHAR(128) NOT NULL,
+  creator_fingerprint VARCHAR(128) NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+  updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+  time_to_live INTERVAL,
+  service VARCHAR(128) NOT NULL,
+  task JSONB NOT NULL,
+  script VARCHAR,
+  status patavi_task_status NOT NULL DEFAULT 'unknown',
+  result JSONB
+);
+
+CREATE FUNCTION patavi_task_timeout() RETURNS trigger
+  LANGUAGE plpgsql
+  AS '
+BEGIN
+  DELETE FROM patavi_task WHERE updated_at < NOW() - time_to_live;
+  RETURN NULL;
+END;
+';
+
+CREATE TRIGGER trigger_patavi_task_timeout
+  AFTER INSERT ON patavi_task
+  EXECUTE PROCEDURE patavi_task_timeout();
+
+CREATE TABLE patavi_file (
+  task_id BIGINT REFERENCES patavi_task(id) ON DELETE CASCADE,
+  path VARCHAR(256),
+  content_type VARCHAR(128),
+  content BYTEA,
+  PRIMARY KEY (task_id, path)
+);
+EOSQL
